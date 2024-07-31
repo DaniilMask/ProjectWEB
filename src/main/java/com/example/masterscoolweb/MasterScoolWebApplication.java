3683dfa3c3f1c0d699bb28d148a5cca1d04ce02c
DaniilMask <danilmask8@gmail.com>
2024-07-31 17:24:45 +0300

    бд подключил

--- src/main/java/com/example/masterscoolweb/MasterScoolWebApplication.java
+++ src/main/java/com/example/masterscoolweb/MasterScoolWebApplication.java
@@ -2,13 +2,15 @@ package com.example.masterscoolweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MasterScoolWebApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(MasterScoolWebApplication.class, args);
+
     }
 
 }
